6944274c56ea36ff1ffbbcdc03cde7ae799623cc
Junghyeon Park <j824h@kaist.ac.kr>
2017-09-07 05:22:02 +0900

    Separate bytes save and text save & change code style
    This change makes version 1.1.
    
    Now the text files are saved using seperate routines for saving bytes
    file.
    save_b is for saving bytes including con image.
    save_s is for saving strings including package details and save info.
    This fixes problems of OS-dependent newlines, and unicode character
    saved as unicode escaped string.
    
    Code style change (Adopting PEP 8, PEP 257)
    PEP 8: Surround top-level function and class definitions with two blank
    lines.
    PEP 257: For consistency, always use """triple double quotes""" around
    docstrings.

--- dccon.py
+++ dccon.py
@@ -3,35 +3,47 @@ import json
 import os
 import time
 
+
 def con_open(no):
-    '''
+    """
     Open the con image file with path number string no.
-    '''
+    """
     con_url = urllib.request.Request('http://dcimg1.dcinside.com/dccon.php?no='+no)
     con_url.add_header('Referer', 'http://dccon.dcinside.com/')
     con_file = urllib.request.urlopen(con_url)
     return con_file
 
-def save(path, bytes):
-    '''
+
+def save_b(path, b):
+    """
     Save bytes at file path.
-    '''
-    with open(path, "wb") as f:
-        f.write(bytes)
+    """
+    with open(path, 'wb') as f:
+        f.write(b)
+        f.close()
+
+
+def save_s(path, string):
+    """
+    Save string as a text file at file path.
+    """
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(string)
         f.close()
 
 def save_con(path, no):
-    '''
+    """
     Save the con image with path number string no.
-    '''
+    """
     con_file = con_open(no)
     name = con_file.info().get('Content-Disposition').split('=')[1]
-    save(path + '\\' + name, con_file.read())
+    save_b(path + '\\' + name, con_file.read())
+
 
 def load_package(idx):
-    '''
+    """
     Load package information from index idx.
-    '''
+    """
     url = 'http://dccon.dcinside.com/index/package_detail'
     headers = {
         'X-Requested-With': 'XMLHttpRequest',
@@ -43,20 +55,23 @@ def load_package(idx):
     package = json.load(package_f)
     return package
 
+
 def save_package(package):
-    '''
+    """
     Save package information, log, con images from loaded package.
-    '''
+    """
     path = package['info']['title']
-    save_info = 'Version: 1.0 \n' + 'Save time: ' + time.strftime("%c")
+    save_info = 'Version: 1.1\n' + 'Save time: ' + time.strftime("%c")
     os.mkdir(path)
-    save(path + '\\' + 'package_details.json', json.dumps(package).encode())
-    save(path + '\\' + 'save_info.txt', save_info.encode())
+    save_s(path + '\\' + 'package_details.json',
+         json.dumps(package, ensure_ascii=False))
+    save_s(path + '\\' + 'save_info.txt', save_info)
     for con in package['detail']:
         save_con(path, con['path'])
 
+
 def quick(idx):
-    '''
+    """
     The one quick command to use.
-    '''
+    """
     save_package(load_package(idx))
